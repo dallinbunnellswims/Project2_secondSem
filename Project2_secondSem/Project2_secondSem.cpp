79fb99a040151c38a82149e137cf46f7c59a89fc
Dallin Bunnell <dallinbunnellswims@hotmail.com>
2016-03-03 10:35:50 -0700

    Added driver
    Now i just need to fill in the method to display it.

--- Project2_secondSem/Project2_secondSem.cpp
+++ Project2_secondSem/Project2_secondSem.cpp
@@ -2,10 +2,41 @@
 //Classes for Book and Author
 
 #include "including.h"
-
+#include "driver.h"
 
 int main()
 {
-    return 0;
+
+	// create a vector for storing the account objects
+	std::vector<Book> myBooks;
+
+	// create three Author objects
+	Author p1("J.K.Rowling", "Edinburgh, Scotland");
+	Author p2("Suzanne Collins", "Connecticut, USA");
+	Author p3("J.R.R. Tolkien", "Bournmouth, England");
+
+	// Create three Book objects
+	Book b1(p1, "Harry Potter and the Sorcerer's Stone", 256, 24.95);
+	Book b2(p2, "Mockingjay", 400, 12.99);
+	Book b3(p3, "The Hobbit", 322, 14.29);
+
+	// add the books to the vector
+	myBooks.push_back(b1);
+	myBooks.push_back(b2);
+	myBooks.push_back(b3);
+
+	// call the displayBooks function to display the books
+	displayBooks(myBooks);
+	std::cout << "\n\n";
+
+	system("PAUSE");
+	return 0;
 }
+
+void displayBooks(const std::vector<Book>& books)
+{
+	// students need to write the code for this function
+}
+
+
 
---
+++ Project2_secondSem/driver.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "including.h"
+#include "Book.h"
+
+// The displayBooks function
+// Purpose: Display all of the data about a book
+// Parameters: A vector of Book objects
+// Returns: none
+void displayBooks(const std::vector<Book>&);
